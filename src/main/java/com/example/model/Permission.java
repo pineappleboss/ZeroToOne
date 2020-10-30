5e24c9f0cb1f9160a6f4abe30fc9e782aa4a2369
tiankaiqiang <tiankaiqiang@hollysys.com>
2020-10-30 18:44:03 +0800

    菜单权限管理

---
+++ src/main/java/com/example/model/Permission.java
@@ -0,0 +1,12 @@
+package com.example.model;
+
+import lombok.Data;
+
+@Data
+public class Permission {
+private int id;
+private String name;
+private String desc;
+private String url;
+private int rid;
+}
---
+++ src/main/java/com/example/model/Role.java
@@ -0,0 +1,11 @@
+package com.example.model;
+
+import lombok.Data;
+
+@Data
+public class Role {
+    private int id;
+    private String name;
+    private int uid;
+
+}
