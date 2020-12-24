9e1c2e24fec08f7fb5762255a0ef547ca25dd01b
tiankaiqiang <374725192@qq.com>
2020-12-24 16:19:27 +0800

    删除多余文件

---
+++ src/main/java/com/example/model/Access.java
@@ -0,0 +1,12 @@
+package com.example.model;
+
+/**
+ * @author tiankaiqiang
+ * @version 1.0
+ * @date 2020/12/23 13:48
+ * @describe
+ */
+public class Access {
+    private String id;
+    private String name;
+}
---
+++ src/main/java/com/example/model/UserRoleMap.java
@@ -0,0 +1,16 @@
+package com.example.model;
+
+import lombok.Data;
+
+/**
+ * @author tiankaiqiang
+ * @version 1.0
+ * @date 2020/12/23 14:02
+ * @describe
+ */
+@Data
+public class UserRoleMap {
+    String id;
+    String uid;
+    String rid;
+}
